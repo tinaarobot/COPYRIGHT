20d743a5eced12665dc869f96835f9fc9a4a8543
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-20 08:17:04 +0530

    Update copyright.py

--- copyright.py
+++ copyright.py
@@ -1,22 +1,16 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message
 import os
-
 
 API_ID = "25450075"
 API_HASH = "278e22b00d6dd565c837405eda49e6f2"
-BOT_TOKEN = os.environ.get("BOT_TOKEN", None) 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 
-
-copyright = Client('my_bot', API_ID=API_ID, API_HASH=API_HASH, BOT_TOKEN=BOT_TOKEN)
-
+copyright = Client('my_bot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 @copyright.on_message(filters.group & filters.text & ~filters.me)
 async def delete_links(client, message):
-    if any(link in message.text for link in ["http", "https", "www."]):
+    if any(link in message.text.lower() for link in ["http", "https", "www."]):
         await message.delete()
-  
-   
-print(f"🎄 𝗦𝗧𝗔𝗥𝗧𝗘𝗗🎄 ")      
-copyright.run()
 
+print(f"🎄 𝗦𝗧𝗔𝗥𝗧𝗘𝗗🎄 ")
+copyright.run()
