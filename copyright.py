2ccf44a63bb2a7f68b9966d4d3d7c5a0ecf19e3d
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-20 08:47:35 +0530

    Update copyright.py

--- copyright.py
+++ copyright.py
@@ -16,45 +16,15 @@ copyright = Client('my_bot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOK
 
 # ----------------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------------------
-
 @copyright.on_message(filters.group & filters.text & ~filters.me)
-async def delete_links(client, message):
-    if any(link in message.text.lower() for link in ["http", "https", "www."]):
+async def delete_links_and_keywords(client, message):
+    keywords = ["NCERT", "XII", "page", "Ans", "meiotic", "divisions", "System.in", "Scanner", "void", "nextInt"]
+    
+    if any(keyword.lower() in message.text.lower() for keyword in keywords) or any(link in message.text.lower() for link in ["http", "https", "www."]):
         await message.delete()
-
+        
 # -------------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------------
-keywords_to_delete = ["NCERT", "XII", "page", "Ans", "meiotic", "divisions", "System.in", "Scanner", "void", "nextInt"]
-
-@copyright.on_edited_message(filters.group & filters.text & ~filters.me)
-async def handle_edited_messages(client, edited_message):
-    await delete_links(client, edited_message)
-
-@copyright.on_message(filters.group & filters.text & ~filters.me)
-async def delete_links(client, message):
-    if any(keyword in message.text for keyword in keywords_to_delete) and len(message.text.split()) > 1:
-        await message.delete()
-
-@copyright.on_edited_message(filters.group & filters.text & ~filters.me)
-async def delete_edited_links(client, edited_message):
-    if any(keyword in edited_message.text for keyword in keywords_to_delete) and len(edited_message.text.split()) > 1:
-        await edited_message.delete()
-
-@copyright.on_message(filters.group & filters.text & ~filters.me)
-async def delete_long_messages(client, message):
-    if len(message.text.split()) >= 1:
-        await message.delete()
-
-@copyright.on_edited_message(filters.group & filters.text & ~filters.me)
-async def delete_edited_long_messages(client, edited_message):
-    if len(edited_message.text.split()) >= 1:
-        await edited_message.delete()
-
-
-
-# ---------------------------
-#--
-
 @copyright.on_edited_message(filters.group & ~filters.me)
 async def delete_edited_messages(client, edited_message):
     await edited_message.delete()
