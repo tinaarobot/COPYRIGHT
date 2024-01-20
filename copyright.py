c78dcd42f3c2f3a160f980486e697efc2d775dc2
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2024-01-19 18:39:13 -0800

    Add files via upload

---
+++ copyright.py
@@ -0,0 +1,27 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+
+
+API_ID = "25450075"
+API_HASH = "278e22b00d6dd565c837405eda49e6f2"
+BOT_TOKEN = os.environ.get("BOT_TOKEN", None) 
+
+
+API_ID = 'YOUR_API_ID'
+API_HASH = 'YOUR_API_HASH'
+
+BOT_TOKEN = 'YOUR_BOT_TOKEN'
+
+copyright = Client('my_bot', API_ID=API_ID, API_HASH=API_HASH, BOT_TOKEN=BOT_TOKEN)
+
+
+@copyright.on_message(filters.group & filters.text & ~filters.me)
+async def delete_links(client, message):
+    if any(link in message.text for link in ["http", "https", "www."]):
+        await message.delete()
+  
+   
+print(f"🎄 𝗦𝗧𝗔𝗥𝗧𝗘𝗗🎄 ")      
+copyright.run()
+
