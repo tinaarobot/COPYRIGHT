8900462d7cbc6a27dc40b227e532d331fe7cdeed
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-06 12:47:08 +0530

    Update __main__.py

--- SHIELD3/__main__.py
+++ SHIELD3/__main__.py
@@ -8,14 +8,26 @@ LOGGER_ID = -1002010924139
 
 loop = asyncio.get_event_loop()
 
-async def royeditx_bot():
+import asyncio
+import importlib
+from pyrogram import idle
+from COPYRIGHT2 import COPYRIGHT2
+from COPYRIGHT2.modules import ALL_MODULES
+
+LOGGER_ID = -1002006622740
+
+loop = asyncio.get_event_loop()
+
+async def roy_boot():
     for all_module in ALL_MODULES:
-        importlib.import_module("SHIELD3.modules." + all_module)
-    print("♥︎ BOT SUCCESSFULLY STARTED.")
+        importlib.import_module("COPYRIGHT2.modules." + all_module)
+    print("𝖻𝗈𝗍 𝗌𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅 𝗌𝗍𝖺𝗋𝗍")
     await idle()
-    print("♥︎ @The_Friendz ♥︎ @H_CC_Help ♥︎ @RoY_EdiTX")
-    await SHIELD3.send_message(LOGGER_ID, "**✦ Bot deployed successfully.**")
+    print("𝖻𝖺𝗁𝖺𝗇𝖼𝗁𝗈𝖽 𝖯𝗂𝗋𝗈 𝖢𝗈𝖽𝖾𝗋 𝗄𝗋𝗅𝗈 𝖾𝖽𝗂𝗍 𝖺𝖺 𝗀𝗒𝖺 𝗇 𝖾𝗋𝗋𝗈𝗋 𝖺𝖺𝖻 𝗃𝖺𝗄𝖾 𝗀𝖺𝗇𝖽 𝗆𝖺𝗋𝗐𝖺𝗈 𝗂𝗌𝗌𝖾 @h_cc_help")
+    await COPYRIGHT2.send_message(LOGGER_ID, "**𝖨 𝖺𝗆 𝖺𝗅𝗂𝗏𝖾 𝖡𝖺𝖻𝗒 𝖸𝗈𝗎𝗋 𝖡𝗈𝗍 𝖲𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅 𝖣𝖾𝗉𝗅𝗈𝗒 \n Mʏ Dᴇᴠᴇʟᴏᴘᴇʀ roy_editx**")
 
 if __name__ == "__main__":
-    loop.run_until_complete(royeditx_bot())
+    loop.run_until_complete(roy_boot())
+
+
 
