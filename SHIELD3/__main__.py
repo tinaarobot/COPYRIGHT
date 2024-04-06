05e6487859aa0fe9a4ae37bb5ec1f9c647ab1241
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-06 12:55:35 +0530

    Update __main__.py

--- SHIELD3/__main__.py
+++ SHIELD3/__main__.py
@@ -8,16 +8,16 @@ LOGGER_ID = -1002010924139
 
 loop = asyncio.get_event_loop()
 
-async def roy_boot():
+async def daxxpapa_boot():
     for all_module in ALL_MODULES:
         importlib.import_module("SHIELD3.modules." + all_module)
     print("𝖻𝗈𝗍 𝗌𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅 𝗌𝗍𝖺𝗋𝗍")
     await idle()
-    print("∆ 𝖻𝖺𝗁𝖺𝗇𝖼𝗁𝗈𝖽 𝖯𝗂𝗋𝗈 𝖢𝗈𝖽𝖾𝗋 𝗄𝗋𝗅𝗈 𝖾𝖽𝗂𝗍 𝖺𝖺 𝗀𝗒𝖺 𝗇 𝖾𝗋𝗋𝗈𝗋 𝖺𝖺𝖻 𝗃𝖺𝗄𝖾 𝗀𝖺𝗇𝖽 𝗆𝖺𝗋𝗐𝖺𝗈 𝗂𝗌𝗌𝖾 @h_cc_help")
-    await SHIELD3.send_message(LOGGER_ID, "**✦ 𝖨 𝖺𝗆 𝖺𝗅𝗂𝗏𝖾 𝖡𝖺𝖻𝗒 𝖸𝗈𝗎𝗋 𝖡𝗈𝗍 𝖲𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅 𝖣𝖾𝗉𝗅𝗈𝗒 \n Mʏ Dᴇᴠᴇʟᴏᴘᴇʀ roy_editx**")
+    print("♥︎ @RoY_EdiTX ♥︎ @H_CC_Help ♥︎ @The_Friendz ")
+    await SHIELD3.send_message(LOGGER_ID, "**✦ ɪ ᴀᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ.\n\n❅ ᴘᴏᴡᴇʀᴅ ʙʏ ➠ ʀ ᴏ ʏ - ᴇ ᴅ ɪ ᴛ x ࿐**")
 
 if __name__ == "__main__":
-    loop.run_until_complete(roy_boot())
+    loop.run_until_complete(daxxpapa_boot())
 
 
 
